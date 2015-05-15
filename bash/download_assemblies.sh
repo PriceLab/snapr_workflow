8043ed354e959c35d8f34e881f2eac10d1da19bc
Buruk Aregawi <burukaregawi@reef.systemsbiology.net>
2015-05-15 11:47:41 -0700

    Fix retry condition on download-assemblies.sh

--- bash/download_assemblies.sh
+++ bash/download_assemblies.sh
@@ -9,7 +9,7 @@ PID=$$
 NUM_RETRIES=0
 touch /tmp/s3-local-file-diff$PID
 echo "empyline" > /tmp/s3-local-file-diff$PID
-while [ -s /tmp/s3-local-file-diff$PID ] || [ $NUM_RETRIES < $MAX_RETRIES]
+while [ -s /tmp/s3-local-file-diff$PID ] && [ $NUM_RETRIES -lt $MAX_RETRIES]
 do
     aws s3 cp $FASTA_SRC /resources/assemblies/ref-genome.fa ;
     aws s3 ls $FASTA_SRC | cut -d " " -f 3  | sort > /tmp/s3-output$PID
@@ -25,7 +25,7 @@ done
 NUM_RETRIES=0
 touch /tmp/s3-local-file-diff$PID
 echo "emptyline" > /tmp/s3-local-file-diff$PID
-while [ -s /tmp/s3-local-file-diff$PID ] || [ $NUM_RETRIES < $MAX_RETRIES]
+while [ -s /tmp/s3-local-file-diff$PID ] && [ $NUM_RETRIES -lt $MAX_RETRIES]
 do
     aws s3 cp $GTF_SRC /resources/assemblies/ref-transcriptome.gtf ;
     aws s3 ls $GTF_SRC | cut -d " " -f 3  | sort > /tmp/s3-output$PID
