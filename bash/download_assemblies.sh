accf4cb07bd6e5a4f866be0724a3a24e8473cdda
Buruk Aregawi <burukaregawi@reef.local>
2015-05-17 16:40:16 -0700

    use awk instead of cut since it avoids certain bugs

--- bash/download_assemblies.sh
+++ bash/download_assemblies.sh
@@ -15,7 +15,7 @@ while [ -s /tmp/s3-local-file-diff$PID ] && [ $NUM_RETRIES -lt $MAX_RETRIES ]
 do
     aws s3 cp $FASTA_SRC /resources/assemblies/ref-genome.fa ;
     aws s3 ls $FASTA_SRC | awk '{print $3}'  | sort > /tmp/s3-output$PID
-    ls -la /resources/assemblies/ref-genome.fa | cut -d " " -f 5 | sort > /tmp/fs-output$PID
+    ls -la /resources/assemblies/ref-genome.fa | awk '{print $3}' | sort > /tmp/fs-output$PID
     diff /tmp/s3-output$PID /tmp/fs-output$PID  > /tmp/s3-local-file-diff$PID
 
     if [ -s /tmp/s3-local-file-diff$PID ]; then
@@ -31,7 +31,7 @@ while [ -s /tmp/s3-local-file-diff$PID ] && [ $NUM_RETRIES -lt $MAX_RETRIES ]
 do
     aws s3 cp $GTF_SRC /resources/assemblies/ref-transcriptome.gtf ;
     aws s3 ls $GTF_SRC | awk '{print $3}'  | sort > /tmp/s3-output$PID
-    ls -la /resources/assemblies/ref-transcriptome.gtf | cut -d " " -f 5 | sort > /tmp/fs-output$PID
+    ls -la /resources/assemblies/ref-transcriptome.gtf | awk '{print $3}' | sort > /tmp/fs-output$PID
     diff /tmp/s3-output$PID /tmp/fs-output$PID  > /tmp/s3-local-file-diff$PID
 
     let NUM_RETRIES++
