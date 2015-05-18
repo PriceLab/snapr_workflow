e03e1b26cedef1b4652249c2d8e1d3939e6d7df6
Buruk Aregawi <burukaregawi@reef.local>
2015-05-17 21:06:17 -0700

    Cleanup download-assemblies.sh and rewrite it so it downloads files in parallel

--- bash/download_assemblies.sh
+++ bash/download_assemblies.sh
@@ -1,43 +1,54 @@
 #!/bin/bash
 
-echo "Beginning to download assemblies from S3"
-
 FASTA_SRC=$1
 GTF_SRC=$2
 
-PID=$$
-
 MAX_RETRIES=5
-NUM_RETRIES=0
-touch /tmp/s3-local-file-diff$PID
-echo "empyline" > /tmp/s3-local-file-diff$PID
-while [ -s /tmp/s3-local-file-diff$PID ] && [ $NUM_RETRIES -lt $MAX_RETRIES ]
-do
-    aws s3 cp $FASTA_SRC /resources/assemblies/ref-genome.fa ;
-    aws s3 ls $FASTA_SRC | awk '{print $3}'  | sort > /tmp/s3-output$PID
-    ls -la /resources/assemblies/ref-genome.fa | awk '{print $3}' | sort > /tmp/fs-output$PID
-    diff /tmp/s3-output$PID /tmp/fs-output$PID  > /tmp/s3-local-file-diff$PID
 
-    if [ -s /tmp/s3-local-file-diff$PID ]; then
-        echo "There was an unknown error in downloading the FASTA file from S3. Retrying. Trial number $NUM_RETRIES"
-    fi
-    let NUM_RETRIES++
-done
+function downloadWithRetries {
+    local REMOTE=$1
+    local LOCAL=$2
 
-NUM_RETRIES=0
-touch /tmp/s3-local-file-diff$PID
-echo "emptyline" > /tmp/s3-local-file-diff$PID
-while [ -s /tmp/s3-local-file-diff$PID ] && [ $NUM_RETRIES -lt $MAX_RETRIES ]
-do
-    aws s3 cp $GTF_SRC /resources/assemblies/ref-transcriptome.gtf ;
-    aws s3 ls $GTF_SRC | awk '{print $3}'  | sort > /tmp/s3-output$PID
-    ls -la /resources/assemblies/ref-transcriptome.gtf | awk '{print $3}' | sort > /tmp/fs-output$PID
-    diff /tmp/s3-output$PID /tmp/fs-output$PID  > /tmp/s3-local-file-diff$PID
+    local RAND=$RANDOM
+    local S3_LS_FILE=/tmp/s3-ls-output$RAND
+    local LOCAL_LS_FILE=/tmp/local-ls-output$RAND
+    local DIFF_FILE=/tmp/s3-local-file-diff$RAND
+    touch $DIFF_FILE
+    echo "file has to start unempty for the while loop below to work" > $DIFF_FILE
+    local NUM_RETRIES=0
+    
+    echo " beginning download of $REMOTE to $LOCAL"
+    while [ -s $DIFF_FILE ] && [ $NUM_RETRIES -lt $MAX_RETRIES ]
+    do
+        # Download file
+        aws s3 cp $REMOTE $LOCAL
 
-    let NUM_RETRIES++
-    if [ -s /tmp/s3-local-file-diff$PID ]; then
-        echo "There was an unknown error in downloading the GTF file from S3. Retrying. Trial number $NUM_RETRIES"
-    fi
-done
+        # Get the size in bytes of remote file and downloaded file
+        aws s3 ls $REMOTE | awk '{print $3}'  | sort > $S3_LS_FILE
+        ls -la $LOCAL | awk '{print $5}' | sort > $LOCAL_LS_FILE
+        # See if there is a difference and save to diff file
+        diff $S3_LS_FILE $LOCAL_LS_FILE  > $DIFF_FILE
 
-echo "Completed downloading assemblies from S3"
+        # If diff file is not empty then that means that there was an error in downloading the files
+        if [ -s $DIFF_FILE ]; then
+            echo "There was an unknown error in downloading $REMOTE from S3 on trial number $NUM_RETRIES. MAX_RETRIES=$MAX_RETRIES"
+        else
+            echo "Completed downloading $REMOTE to $LOCAL"
+        fi
+        let NUM_RETRIES++
+    done
+}
+
+
+echo "Kicking off download for $FASTA_SRC and $GTF_SRC."
+
+# Kick off gtf file in background to save time and because we know it will complete downloading before the fasta file
+# Capture process output into a file we can print to this process's stdout when done
+TMP_OUTPUT_FILE=/tmp/background-proc-output$RANDOM
+downloadWithRetries $GTF_SRC /resources/assemblies/ref-transcriptome.gtf > $TMP_OUTPUT_FILE 2>&1
+
+downloadWithRetries $FASTA_SRC /resources/assemblies/ref-genome.fa
+
+cat $TMP_OUTPUT_FILE
+
+rm $TMP_OUTPUT_FILE
